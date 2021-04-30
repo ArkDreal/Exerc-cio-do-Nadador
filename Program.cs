76a2c0d26fc12d9f7f0477915258134a949359c7
ArkDreal <andrey.o.leal@gmail.com>
2021-04-30 14:39:11 -0300

    Exercício do Nadador concluído

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Exercicio_Nadador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é a sua idade?");
+            int idadePessoa = int.Parse(Console.ReadLine());
+
+            if (idadePessoa < 5)
+            {
+                Console.WriteLine("Você não é classificável ");
+            }
+
+            else
+            {
+                if (idadePessoa <= 7)
+                {
+                    Console.WriteLine("Sua categoria é: Infantil A");
+                }
+
+                else
+                {
+                    if (idadePessoa <= 10)
+                    {
+                        Console.WriteLine("Sua categoria é: Infantil B");
+                    }
+
+
+                    else
+                    {
+                        if (idadePessoa <= 13)
+                        {
+                            Console.WriteLine("Sua categoria é: juvenil A");
+                        }
+
+                        else
+                        {
+                            if (idadePessoa <= 17)
+                            {
+                                Console.WriteLine("Sua categoria é: Juvenil B");
+                            }
+
+                            else
+                            {
+                                if (idadePessoa >= 18)
+                                {
+                                    Console.WriteLine("Sua categoria é: Sênior");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
